6f61d9b48662b8a2dfe2e190480af2aeabd577e6
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-04-12 09:14:25 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dnt-root',
   templateUrl: './app.component.html',
+  // template: `<h1>Hello from Angular</h1>
+  //   <h2>Hello world!<h2>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
