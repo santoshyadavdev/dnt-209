78b209477167817b16f36b07be165d1e7d222834
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-04-19 09:07:13 +0530

    added contentchildren example

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentChecked, AfterContentInit } from '@angular/core';
+import { DepartmentComponent } from '../department/department.component';
 
 @Component({
   selector: 'dnt-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent implements OnInit {
+export class ContainerComponent implements OnInit, AfterContentInit {
+
+  @ContentChild(DepartmentComponent) departmentComponent : DepartmentComponent;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit() {
+    this.departmentComponent.visible = false;
   }
 
 }
